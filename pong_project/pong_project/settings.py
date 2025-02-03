23351b157a3c9c9686112c00c706922ecb9929f6
Jeffneck <hugot.anglade@gmail.com>
2025-02-03 10:08:37 +0100

    adding ipconfig of a pc in 42

--- pong_project/pong_project/settings.py
+++ pong_project/pong_project/settings.py
@@ -143,6 +143,7 @@ CSRF_TRUSTED_ORIGINS = [
     'http://192.168.1.138:8080',
     'http://192.168.1.138:443',
     'https://192.168.1.176:443',
+    'https://10.12.6.7:443',
     # 'https://votre-domaine.com',  # Ajoutez votre domaine en production
 ]
 CSRF_COOKIE_SAMESITE = 'Lax'
